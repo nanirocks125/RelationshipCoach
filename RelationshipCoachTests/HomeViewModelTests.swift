f7e76277c0764387c24806d2ba38fc8228125376
nanirocks125 <nanirocks125@gmail.com>
2025-07-13 22:20:53 +0530

    Updated test cases

--- RelationshipCoachTests/HomeViewModelTests.swift
+++ RelationshipCoachTests/HomeViewModelTests.swift
@@ -28,5 +28,53 @@ final class HomeViewModelTests {
     func test_prepareSections_populatesSections(gender: Gender) {
         sut.prepareSections(for: gender)
         #expect(sut.sections.count == 3)
+        
+        // Test for section 1
+        let section1 = sut.sections[0]
+        #expect(section1.type == .story)
+        #expect(section1.items.count == gender.expectedStoryItems)
+        
+        if section1.type == .story && gender == .male {
+            let items = section1.items
+            #expect(items[0].story == .herFault)
+            #expect(items[1].story == .myFault)
+            #expect(items[2].story == .toxic)
+            #expect(items[3].story == .giving)
+            #expect(items[4].story == .sweet)
+            #expect(items[5].story == nil)
+        }
+        
+        if section1.type == .story && gender == .female {
+            let items = section1.items
+            #expect(items[0].story == .hisFault)
+            #expect(items[1].story == .myFault)
+            #expect(items[2].story == .toxic)
+            #expect(items[3].story == .receiving)
+            #expect(items[4].story == .sweet)
+            #expect(items[5].story == nil)
+        }
+        
+        // Test for section 1
+        let section2 = sut.sections[1]
+        #expect(section2.type == .instagram)
+        #expect(section2.items.count == 1)
+        
+        // Test for section 1
+        let section3 = sut.sections[2]
+        #expect(section3.type == .preference)
+        #expect(section3.items.count == 1)
+    }
+}
+
+extension Gender {
+    var expectedStoryItems: Int {
+        switch self {
+        case .male:
+            return 6
+        case .female:
+            return 6
+        case .none:
+            return 1
+        }
     }
 }
