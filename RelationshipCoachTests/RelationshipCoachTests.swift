bf2c24faee822951e4f66e8a9d2af961883a3415
nanirocks125 <nanirocks125@gmail.com>
2025-07-13 15:20:12 +0530

    Test module is added

---
+++ RelationshipCoachTests/RelationshipCoachTests.swift
@@ -0,0 +1,16 @@
+//
+//  RelationshipCoachTests.swift
+//  RelationshipCoachTests
+//
+//  Created by Manikanta Nandam on 13/07/25.
+//
+
+import Testing
+
+struct RelationshipCoachTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
